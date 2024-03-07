620e2073e2e85f06ee46076188ec94439e3e6c78
Brock Britton <brockbritton@outlook.com>
2024-03-07 10:50:51 -0800

    added modularization

---
+++ weather.js
@@ -0,0 +1,33 @@
+
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const weather_key = process.env.WEATHERBIT_API_KEY;
+
+class Forecast {
+  constructor(date, description, high, low) {
+    this.date = date;
+    this.description = description;
+    this.high = high;
+    this.low = low;
+  }
+}
+
+async function fetchWeatherData(raw_lat_lon) {
+  try {
+    const lat = Number(parseFloat(raw_lat_lon.split('_')[0]).toFixed(4));
+    const lon = Number(parseFloat(raw_lat_lon.split('_')[1]).toFixed(4));
+    let weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${weather_key}&days=6&units=I`);
+    return weatherData.data.data.map((values) => {
+      return new Forecast(values.datetime, values.weather.description, values.max_temp, values.min_temp);
+    });
+  } catch (error) {
+    console.log('Cannot get weather data');
+    return [];
+  }
+}
+
+module.exports = {
+  fetchWeatherData
+};
