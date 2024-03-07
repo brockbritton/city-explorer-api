b8254bb096617c40b783f76c92a118732d1a450e
Brock Britton <brockbritton@outlook.com>
2024-03-06 16:35:52 -0800

    server uses live data

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 'use strict';
 
+const axios = require('axios');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors'); // cross origin resource sharing tool
-const weatherData = require('./data/weather.json');
 
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const weather_key = process.env.WEATHERBIT_API_KEY;
 app.use(cors());
 
 class Forecast {
@@ -25,20 +26,15 @@ app.get('/', (req, res) => {
   res.send('Hello, Express!');
 });
 
-app.get('/weather/:lat_lon', (req, res) => {
-  const lat = req.params.lat_lon.split('_')[0];
-  const lon = req.params.lat_lon.split('_')[1];
-  const foundCity = weatherData.find(city => city.lat === lat && city.lon === lon);
-
-  if (!foundCity) {
-    return res.status(404).json({error: 'City not found. Please search for Seattle, Paris, or Amman.'});
-  } else {
-    let weatherDex = foundCity.data.map((values) => {
-      return new Forecast(values.datetime, values.weather.description, values.max_temp, values.min_temp);
-    });
-
-    res.send(weatherDex);
-  }
+app.get('/weather/:lat_lon', async (req, res) => {
+  const lat = Number(parseFloat(req.params.lat_lon.split('_')[0]).toFixed(4));
+  const lon = Number(parseFloat(req.params.lat_lon.split('_')[1]).toFixed(4));
+  let weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${weather_key}&days=6&units=I`);
+  let weatherDex = weatherData.data.data.map((values) => {
+    return new Forecast(values.datetime, values.weather.description, values.max_temp, values.min_temp);
+  });
+  res.send(weatherDex);
+  
 });
 
 app.listen(port, () => {
